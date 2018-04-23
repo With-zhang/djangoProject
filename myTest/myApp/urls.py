9ca355385d92f2eca88cb1a3b74d4c2932eea92e
With-zhang <withzhang@outlook.com>
2018-04-24 00:52:59 +0800

    页面跳转

--- myTest/myApp/urls.py
+++ myTest/myApp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(r'myApp1/', views.one),
+    path(r'elements/', views.elements),
 ]
--- myTest/myApp/views.py
+++ myTest/myApp/views.py
@@ -8,6 +8,19 @@ import re
 
 def one(request):
     template = loader.get_template(r'myApp/index.html')
+    getUserName = request.GET.get('username')
+    file = r'myApp/log.txt'
+    openFile = open(file,'w')
+    openFile.write(getUserName)
+    openFile.close
+    # template = loader.get_template(r'C:/Users/zhang/Desktop/20171013021024.html')
+    context = {
+        'tnames' : ['a','b']
+    }
+    return HttpResponse(template.render(context,request))
+
+def elements(request):
+    template = loader.get_template(r'myApp/elements.html')
     # template = loader.get_template(r'C:/Users/zhang/Desktop/20171013021024.html')
     context = {
         'tnames' : ['a','b']
